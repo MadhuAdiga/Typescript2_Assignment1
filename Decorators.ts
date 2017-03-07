164d03177b15ceab1ab80ca5cea154fbf6edcbb9
MadhuAdiga <madhu.vah@gmail.com>
2017-03-07 08:16:27 +0530

    Assignment1

--- Decorators.ts
+++ Decorators.ts
@@ -1,19 +1,25 @@
-function f() {
-    console.log("f(): evaluated");
+function f() { // Define the decorator f() 
+    console.log("f(): evaluated"); //Gets evaluated first
     return function (target, propertyKey: string, descriptor: PropertyDescriptor) {
-        console.log("f(): called");
+        console.log("f(): called"); //Gets evaluated fourth
     }
 }
 
-function g() {
-    console.log("g(): evaluated");
+function g() { // Define the decorator g() 
+    console.log("g(): evaluated"); //Gets evaluated second
     return function (target, propertyKey: string, descriptor: PropertyDescriptor) {
-        console.log("g(): called");
+        console.log("g(): called"); //Gets evaluated third
     }
 }
 
-class C {
+class C { //Defines a class C consiting of the above defined decorator 
     @f()
     @g()
     method() {}
 }
+
+//Logs the below:
+// f(): evaluated
+// g(): evaluated
+// g(): called
+// f(): called
